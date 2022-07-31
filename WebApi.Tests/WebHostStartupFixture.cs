e31c40a6269f3ad9eaba9b8e50c4da8aecb0454f
Remko van der Meulen <remko.van.der.meulen@wolterskluwer.com>
2022-07-31 14:57:49 +0200

    Hello World web app with integration test

---
+++ WebApi.Tests/WebHostStartupFixture.cs
@@ -0,0 +1,27 @@
+using System.Net;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+
+using NUnit.Framework;
+
+namespace WebApi.Tests;
+
+class WebHostStartupFixture
+{
+    readonly WebApplicationFactory<WebHostStartup> factory = new();
+
+    [Test]
+    public async Task Should_get_content()
+    {
+        using var client = factory.CreateClient();
+
+        var response = await client.GetAsync("/");
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.That(content, Is.EqualTo("Hello World!"));
+    }
+}
